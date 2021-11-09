4d5db94f6a351d4cd60dc9313ad348d23cf6e9cf
Abner Ballardo <abner@miblum.com>
2021-11-09 16:39:45 -0500

    Inital version

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {userService} from "./service";
+import {User} from "./domain/User";
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState<User>();
+
+  useEffect(() => {
+    const findRandomUser = async () => setCurrentUser(await userService.findRandomUser())
+    findRandomUser();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </header>
+      <p>
+        Name: {currentUser?.name} <br/>
+        Last Name: {currentUser?.lastName} <br/>
+        Email: {currentUser?.email} <br/>
+      </p>
+    </div>
+  );
+}
+
+export default App;
